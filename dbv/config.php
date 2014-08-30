1e0fc81f3b69f6fab345bcd95c30ddc19bca39b4
wern <ancheta.wern@gmail.com>
2014-08-30 09:49:01 +0800

    add code for dbv

---
+++ dbv/config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Your database authentication information goes here
+ * @see http://dbv.vizuina.com/documentation/
+ */
+define('DB_HOST', 'localhost');
+define('DB_PORT', 3306);
+define('DB_USERNAME', 'my_username');
+define('DB_PASSWORD', 'my_password');
+define('DB_NAME', 'my_database');
+
+/**
+ * Authentication data for access to DBV itself
+ * If you leave any of the two constants below blank, authentication will be disabled
+ * @see http://dbv.vizuina.com/documentation/#optional-settings
+ */
+define('DBV_USERNAME', 'my_username');
+define('DBV_PASSWORD', 'my_password');
+?>
